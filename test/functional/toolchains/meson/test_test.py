11bfccc1764a48a7f40d50306ddf837072344aed
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-12 14:01:33 -0400

    RabeyaMuna__diff__id_523

--- test/functional/toolchains/meson/test_test.py
+++ test/functional/toolchains/meson/test_test.py
@@ -1,10 +1,10 @@
 import os
+import textwrap
+from test.functional.toolchains.meson._base import TestMesonBase
 
 import pytest
-import textwrap
 
 from conan.test.assets.sources import gen_function_cpp
-from test.functional.toolchains.meson._base import TestMesonBase
 
 
 @pytest.mark.tool("pkg_config")
@@ -47,15 +47,24 @@ class MesonTest(TestMesonBase):
                 meson.test()
         """)
 
+    @pytest.mark.tool("cmake")
     def test_reuse(self):
         self.t.run("new cmake_lib -d name=hello -d version=0.1")
 
-        test_package_cpp = gen_function_cpp(name="main", includes=["hello"], calls=["hello"])
+        test_package_cpp = gen_function_cpp(
+            name="main", includes=["hello"], calls=["hello"]
+        )
 
-        self.t.save({os.path.join("test_package", "conanfile.py"): self._test_package_conanfile_py,
-                     os.path.join("test_package", "meson.build"): self._test_package_meson_build,
-                     os.path.join("test_package", "test_package.cpp"): test_package_cpp})
+        self.t.save(
+            {
+                os.path.join(
+                    "test_package", "conanfile.py"
+                ): self._test_package_conanfile_py,
+                os.path.join(
+                    "test_package", "meson.build"
+                ): self._test_package_meson_build,
+                os.path.join("test_package", "test_package.cpp"): test_package_cpp,
+            }
+        )
 
         self.t.run("create . --name=hello --version=0.1")
-
-        self._check_binary()
